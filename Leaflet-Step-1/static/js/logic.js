5f14096438058cfa4392684b8dfd6d2fce2964aa
Serapbasaran <serap.cbasaran@gmail.com>
2021-01-21 12:35:03 -0500

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,7 +1,23 @@
+// Create a map object
+var myMap = L.map("map", {
+    center: [15.5994, -28.6731], 
+    zoom: 5
+  });
+  
+  // Create a tile layer 
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
 // Set the url query to make an API call to get the eaerthquake 
 // records with M4.5+ last 30 days on the earth 
 
-var queryURL =" https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"
+var queryURL =" https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -9,21 +25,4 @@ d3.json(queryUrl, function(data) {
     createFeatures(data.features);
   });
 
-// Create a map object
-var myMap = L.map("map", {
-    center: [15.5994, -28.6731], 
-    zoom: 5
-  });
-  
 
-
-
-function createFeatures(earthquakeData) {
-
-    // Define a function we want to run once for each feature in the features array
-    // Give each feature a popup describing the place and time of the earthquake
-    function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-    }
-  
