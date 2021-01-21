61f7581285e98e052a69485ceca428af2ddd6242
Serapbasaran <serap.cbasaran@gmail.com>
2021-01-21 13:06:02 -0500

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,6 @@
 // Create a map object
 var myMap = L.map("map", {
-    center: [15.5994, -28.6731], 
+    center: [37.09, -95.71],
     zoom: 5
   });
   
@@ -21,8 +21,12 @@ var queryURL =" https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-    // Once we get a response, send the data.features object to the createFeatures function
-    createFeatures(data.features);
-  });
+    console.log(data)
+    var FeaturesArray = data.features
+    for (var i = 0; i < FeaturesArray.length; i++) {
+    var Coordinates = FeaturesArray[i].geometry.coordinates
+    var Time = FeaturesArray[i].properties.time
+    var Magnitude = FeaturesArray[i].properties.mag
 
+        
 
